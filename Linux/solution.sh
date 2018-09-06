23ce75fe3f81d215b257527d345259e4b4543dec
Teodor-Hr <26335078+teodtedo771@users.noreply.github.com>
2018-09-07 01:15:18 +0300

    Fixed syntax error

--- Linux/solution.sh
+++ Linux/solution.sh
@@ -8,10 +8,11 @@ cp text.txt /tmp/text.txt;
 cat $PARAMETERS /tmp/vars | bash > /tmp/vars_values;
 
 index=1;
-for var in cat /tmp/vars_names;
+for var in `cat /tmp/vars_names`;
 do
     sed -i -r "s/@$var@/`head -$index /tmp/vars_values | tail -1`/g" /tmp/text.txt;
     index=$(($index+1));
 done
 
 cat /tmp/text.txt;
+#cat /tmp/vars_names;
