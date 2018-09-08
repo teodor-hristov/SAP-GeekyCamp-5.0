77c2903b868983c7c1d82b14a4c80cb2967c07cc
Teodor Hristov <26335078+teodtedo771@users.noreply.github.com>
2018-09-09 01:35:36 +0300

    Initial commit

--- public/util.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
--- src/File.java
+++
@@ -1,9 +0,0 @@
-import java.util.HashMap;
-
-public class File {
-    private HashMap< String, File > files = new HashMap < > ();
-    private String content = "";
-
-    public File() {
-    }
-}
