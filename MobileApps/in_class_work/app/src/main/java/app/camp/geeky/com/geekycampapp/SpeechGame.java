2c2d4ee61941b93e539b02ea047ee107fc5d0920
Teodor Hristov <26335078+teodtedo771@users.noreply.github.com>
2018-09-09 01:13:00 +0300

    Cloning new repo

---
+++ MobileApps/in_class_work/app/src/main/java/app/camp/geeky/com/geekycampapp/SpeechGame.java
@@ -0,0 +1,94 @@
+package app.camp.geeky.com.geekycampapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class SpeechGame extends AppCompatActivity implements View.OnClickListener {
+
+    TextToSpeech toSpeech;
+    boolean isInitialised;
+
+    TextView tvSpeech;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_speech_game);
+
+        tvSpeech = findViewById(R.id.tvSpeech);
+
+        findViewById(R.id.bSpeak).setOnClickListener(this);
+        findViewById(R.id.bDictate).setOnClickListener(this);
+
+        initSpeech();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.bSpeak:
+                speakPressed();
+                break;
+            case R.id.bDictate:
+                dictatePressed();
+                break;
+        }
+    }
+
+    public void initSpeech() {
+        isInitialised = false;
+        toSpeech = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+                    isInitialised = true;
+                }
+            }
+        });
+    }
+
+    public void speakPressed() {
+        Toast.makeText(this, "Speak Pressed", Toast.LENGTH_SHORT).show();
+
+        Intent i = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        i.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        i.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.ENGLISH);
+
+        startActivityForResult(i, 200);
+    }
+
+    public void dictatePressed() {
+        Toast.makeText(this, "Dictate Pressed", Toast.LENGTH_SHORT).show();
+
+        if (isInitialised) {
+            toSpeech.speak("Hello Geeky Camp!", TextToSpeech.QUEUE_FLUSH, null, null);
+        } else {
+            Toast.makeText(this, "Object not initialized properly!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == 200) {
+            if (resultCode == RESULT_OK && data != null) {
+                ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                String sResult = result.get(0);
+                tvSpeech.setText(sResult);
+
+
+
+        }
+
+    }
+}
