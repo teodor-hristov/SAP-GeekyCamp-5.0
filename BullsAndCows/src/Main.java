f3c69f025c8ac6b30f1fe7b56c87e15a4c8d6bb0
Teodor Hristov <26335078+teodtedo771@users.noreply.github.com>
2018-09-09 21:23:15 +0300

    I rly wanna think that i fixed this problem

---
+++ BullsAndCows/src/Main.java
@@ -0,0 +1,17 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String num = in.nextLine();
+        Computer pc1 = new Computer("Pesho");
+        Player player1 = new Player(num, "Tedo");
+        Game game1 = new Game(player1, pc1);
+        System.out.println(game1.getStatus());
+    }
+}
---
+++ BullsAndCows/src/Player.java
@@ -0,0 +1,26 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
+public class Player {
+    private String number;
+    private String playerName;
+
+    Player(String number, String playerName) {
+        this.number = number;
+        this.playerName = playerName;
+    }
+
+    public String getPlayerName() {
+        return this.playerName;
+    }
+
+    public String getNumber() {
+        return this.number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+}
