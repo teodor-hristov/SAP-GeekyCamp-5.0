edc75dde94f4561c7f9c08929afe4a17eafd0a7d
Teodor Hristov <26335078+teodtedo771@users.noreply.github.com>
2018-09-11 22:02:00 +0300

    Hate android studio (#9)

--- MobileApps/in_class_work/app/src/main/java/app/camp/geeky/com/geekycampapp/SpeechGame.java
+++ MobileApps/in_class_work/app/src/main/java/app/camp/geeky/com/geekycampapp/SpeechGame.java
@@ -1,26 +1,39 @@
 package app.camp.geeky.com.geekycampapp;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.common.api.GoogleApi;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 public class SpeechGame extends AppCompatActivity implements View.OnClickListener {
 
-    TextToSpeech toSpeech;
+    DBHelper mydb;
+    SQLiteDatabase pesho;
+    int idToUpdate =0;
     boolean isInitialised;
+    TextToSpeech toSpeech;
+
+    EditText email;
+    EditText question;
+    EditText answer;
 
     TextView tvSpeech;
     TextView yesOrNo;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,11 +43,18 @@ public class SpeechGame extends AppCompatActivity implements View.OnClickListene
         tvSpeech = findViewById(R.id.tvSpeech);
         yesOrNo = findViewById(R.id.yesOrNoProperty);
 
+        email = findViewById(R.id.emailField);
+        question = findViewById(R.id.questionField);
+        answer = findViewById(R.id.answerField);
+
         findViewById(R.id.bSpeak).setOnClickListener(this);
         findViewById(R.id.bDictate).setOnClickListener(this);
         findViewById(R.id.bFindOut).setOnClickListener(this);
-
+        findViewById(R.id.bValidate).setOnClickListener(this);
         initSpeech();
+        mydb = new DBHelper(this);
+        //pesho = new SQLiteDatabase(); не мога да се справя, опитах какво ли не за да го фиксна, но не..
+
     }
 
     @Override
@@ -48,6 +68,12 @@ public class SpeechGame extends AppCompatActivity implements View.OnClickListene
                 break;
             case R.id.bFindOut:
                 speakPressed();
+                break;
+            case R.id.bValidate:
+                validateEmail();
+                break;
+            case R.id.bInsert:
+                insert();
                 break;
         }
     }
@@ -85,6 +111,33 @@ public class SpeechGame extends AppCompatActivity implements View.OnClickListene
         }
     }
 
+    public void validateEmail() {
+        final Pattern EMAIL_REGEX = Pattern.compile("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", Pattern.CASE_INSENSITIVE);
+        Toast.makeText(this, "Validate Pressed", Toast.LENGTH_SHORT).show();
+
+        String value;
+        value = this.email.getText().toString();
+        if (EMAIL_REGEX.matcher(value).matches()) {
+            Toast.makeText(this, "Valid", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Not valid", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+    public void insert() {
+        final Pattern EMAIL_REGEX = Pattern.compile("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", Pattern.CASE_INSENSITIVE);
+        Toast.makeText(this, "Validate Pressed", Toast.LENGTH_SHORT).show();
+
+        String value;
+        value = this.email.getText().toString();
+        if (EMAIL_REGEX.matcher(value).matches()) {
+            Toast.makeText(this, "Valid", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Not valid", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         Map<String, String> questionsAndAnswers;
@@ -95,8 +148,8 @@ public class SpeechGame extends AppCompatActivity implements View.OnClickListene
                 tvSpeech.setText(sResult);
 
                 questionsAndAnswers = new HashMap<String, String>();
-                questionsAndAnswers.put("how old are you","15");
-                questionsAndAnswers.put("which brand is your laptop from","asus");
+                questionsAndAnswers.put("how old are you", "15");
+                questionsAndAnswers.put("which brand is your laptop from", "asus");
 
                 if (questionsAndAnswers.containsKey(sResult.toLowerCase())) {
                     yesOrNo.setText(questionsAndAnswers.get(sResult.toLowerCase()));
